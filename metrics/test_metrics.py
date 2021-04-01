c490cae6f92cd98cc1ca927bdea7533cf7f15554
Lee <gdpp4352@gmail.com>
2021-04-01 15:28:52 -0600

    simplify test_metrics.py

--- metrics/test_metrics.py
+++ metrics/test_metrics.py
@@ -13,75 +13,60 @@ x_eg = pd.Series([2, 2, 2, 2, 16])
 y_eg = pd.Series([4, 5, 6, -7, 8])
 
 
+def read_metric(metric):
+
+    return eval_tools.get_module_class(test_dir, metric)()
+
+
 def test_bias():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'bias')()
-
-    assert metric_obj.compute(5, 4) == -1
+    assert read_metric('bias').compute(5, 4) == -1
 
 
 def test_series_bias():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'bias')()
-
-    assert metric_obj.compute(x_eg, y_eg) == -1.6
+    assert read_metric('bias').compute(x_eg, y_eg) == -1.6
 
 
 def test_bias_pct():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'bias_pct')()
-
-    assert metric_obj.compute(5, 4) == -20
+    assert read_metric('bias_pct').compute(5, 4) == -20
 
 
 def test_series_bias_pct():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'bias_pct')()
-
-    assert metric_obj.compute(x_eg, y_eg) == -10
+    assert read_metric('bias_pct').compute(x_eg, y_eg) == -10
 
 
 def test_mae():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'mae')()
-
-    assert metric_obj.compute(5, 4) == 1
+    assert read_metric('mae').compute(5, 4) == 1
 
 
 def test_series_mae():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'mae')()
-
-    assert metric_obj.compute(x_eg, y_eg) == 5.2
+    assert read_metric('mae').compute(x_eg, y_eg) == 5.2
 
 
 def test_mae_pct():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'mae_pct')()
-
-    assert metric_obj.compute(5, 4) == 20
+    assert read_metric('mae_pct').compute(5, 4) == 20
 
 
 def test_series_mae_pct():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'mae_pct')()
-
-    assert metric_obj.compute(x_eg, y_eg) == 190
+    assert read_metric('mae_pct').compute(x_eg, y_eg) == 190
 
 
 def test_series_rmse():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'rmse')()
-
-    result = metric_obj.compute(x_eg, y_eg)
+    result = read_metric('rmse').compute(x_eg, y_eg)
 
     assert math.isclose(result, 5.899, rel_tol=1e-4)
 
 
 def test_series_crmse():
 
-    metric_obj = eval_tools.get_module_class(test_dir, 'crmse')()
-
-    result = metric_obj.compute(x_eg, y_eg)
+    result = read_metric('crmse').compute(x_eg, y_eg)
 
     assert math.isclose(result, 5.678, rel_tol=1e-4)
