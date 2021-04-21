35335cdcc916901e20dd756941d25c4afa391a81
Lee <gdpp4352@gmail.com>
2021-04-21 14:10:47 -0600

    convert wd into unit vectors

--- tools/eval_tools.py
+++ tools/eval_tools.py
@@ -1,6 +1,8 @@
 # This script contains tools for the codebase.
 
 import importlib
+import math
+import numpy as np
 
 
 def get_module_class(d, c):
@@ -32,3 +34,38 @@ def append_results(results, base, c, conf):
                    )
 
     return results
+
+
+def calculate_angle_diff(x_i, y_i):
+    """Calculate angle difference between two wind directions.
+    Converting wind direction angle into u and v unit vectors,
+    to avoid 0-360 degree split.
+    It only yields positive angle difference.
+    """
+
+    u_x = -math.sin(math.radians(x_i))
+    v_x = -math.cos(math.radians(x_i))
+
+    u_y = -math.sin(math.radians(y_i))
+    v_y = -math.cos(math.radians(y_i))
+
+    if u_x == u_y and v_x == v_y:
+        angle = 0
+    else:
+        angle = math.degrees(math.acos((u_x*u_y + v_x*v_y)))
+
+    return angle
+
+
+def get_wd_angle_diff_series(x, y):
+    """Convert x and y time series into one series of wind direction
+    difference in degrees.
+    """
+
+    angle_series = np.empty(len(x))
+
+    for i, (x_i, y_i) in enumerate(zip(x, y)):
+
+        angle_series[i] = calculate_angle_diff(x_i, y_i)
+
+    return angle_series
