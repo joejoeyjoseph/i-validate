aa8b1be5405a3bad17acdee4ea421eb0d236c4a2
Lee <gdpp4352@gmail.com>
2021-05-04 14:04:31 -0600

    add ramp metrics & contingency table

--- ivalidate.py
+++ ivalidate.py
@@ -34,6 +34,15 @@ def compare(config=None):
     comp = conf['comp']
     p_curve = conf['power_curve']
 
+    if 'ramps' in conf:
+        print(conf['ramps']['definition'])
+        # eval_tools.get_module_class('ramps', 'crosscheck_ts')(conf)
+        ramps = [eval_tools.get_module_class('ramps', r)()
+                 for r in conf['ramps']['definition']]
+        print(ramps)
+    # print((conf['ramps']))
+    # print(conf in globals())
+
     print('validation start time:', conf['time']['window']['start'])
     print('validation end time:', conf['time']['window']['end'])
     print('location:', conf['location'])
@@ -43,6 +52,7 @@ def compare(config=None):
     # Load modules
     metrics = [eval_tools.get_module_class('metrics', m)()
                for m in conf['metrics']]
+    print(metrics)
     crosscheck_ts = eval_tools.get_module_class('qc', 'crosscheck_ts')(conf)
     plotting = eval_tools.get_module_class('plotting', 'plot_data')(conf)
 
@@ -92,9 +102,62 @@ def compare(config=None):
             plotting.plot_histogram(combine_df, lev)
             plotting.plot_pair_scatter(combine_df, lev)
 
+            # import datetime
+            ramp_data = cal_print_metrics.remove_na(combine_df, ramp_txt=True)
+
+            ramp_data_dn = ramp_data.copy()
+            ramp_data_dn.index = ramp_data_dn.index - pd.to_timedelta(
+                str(conf['ramps']['duration']))
+
+            ramp_df = (ramp_data_dn - ramp_data).dropna()
+            zeros_col = np.zeros(len(ramp_df))
+            ramp_df['abs_diff_base'] = zeros_col
+            ramp_df['abs_diff_comp'] = zeros_col
+
+            ramp_df.loc[abs(ramp_df['sodar_ws'])
+                > conf['ramps']['magnitude'], ['abs_diff_base']] = 1
+            ramp_df.loc[abs(ramp_df['wrf_ws'])
+                > conf['ramps']['magnitude'], ['abs_diff_comp']] = 1
+
+            false_col = np.zeros(len(ramp_df), dtype=bool)
+            ramp_df['true_positive'] = false_col
+            ramp_df['false_positive'] = false_col
+            ramp_df['false_negative'] = false_col
+            ramp_df['true_negative'] = false_col
+
+            ramp_df.loc[((ramp_df['abs_diff_base'] != 0) & (ramp_df['abs_diff_comp'] != 0)), ['true_positive']] = True
+            ramp_df.loc[((ramp_df['abs_diff_base'] == 0) & (ramp_df['abs_diff_comp'] != 0)), ['false_positive']] = True
+            ramp_df.loc[((ramp_df['abs_diff_base'] != 0) & (ramp_df['abs_diff_comp'] == 0)), ['false_negative']] = True
+            ramp_df.loc[((ramp_df['abs_diff_base'] == 0) & (ramp_df['abs_diff_comp'] == 0)), ['true_negative']] = True
+
+            for i, row in ramp_df.iterrows():
+                assert np.sum([row['true_positive'], row['false_positive'], row['false_negative'], row['true_negative']]) == 1
+
+            true_pos = ramp_df['true_positive'].sum()
+            false_pos = ramp_df['false_positive'].sum()
+            false_neg = ramp_df['false_negative'].sum()
+            true_neg = ramp_df['true_negative'].sum()
+
+            assert true_pos+false_pos+false_neg+true_neg == len(ramp_df)
+
+            pod = true_pos/(true_pos+false_neg)
+            print(pod)
+            csi = true_pos/(true_pos+false_pos+false_neg)
+            print(csi)
+            fbias = (true_pos+false_pos)/(true_pos+false_neg)
+            far = false_pos/(true_pos+false_pos)
+            print(fbias, far)
+
+            fa = true_pos/(true_pos+false_pos)
+
+            # TF = true_pos, FF = false_pos, MR = false_neg, TN = true_neg
+            pss = ((true_pos*true_neg) - (false_pos*false_neg)) / ((true_pos+false_neg) * (false_pos+true_neg))
+            print(fa, pss)
+
             combine_df.columns = pd.MultiIndex.from_product([[lev],
                                                             combine_df.columns]
                                                             )
+
 
             if all_lev_df.empty:
                 all_lev_df = all_lev_df.append(combine_df)
