f9da041c06e7b5941fe2fc551a86b1fa210c410c
Lee <gdpp4352@gmail.com>
2021-05-11 15:23:44 -0600

    add printing & plotting details

--- ramps/r_magnitude.py
+++ ramps/r_magnitude.py
@@ -12,16 +12,24 @@ class r_magnitude:
 
     def __init__(self, conf, c, ramp_data):
 
-        self.conf = conf
         self.base_var = conf['base']['target_var']
         self.comp_var = c['target_var']
+        self.ramps = conf['ramps']
         self.ramp_data = ramp_data
+        self.reference = conf['reference']
 
     def get_rampdf(self):
 
+        print()
+        print('classfy a ramp event when '+self.reference['var']
+              + ' exceeds |'+str(self.ramps['magnitude'])
+              + '| '+self.reference['units']+' in a window of '
+              + self.ramps['duration']
+              )
+
         ramp_data_dn = self.ramp_data.copy()
         ramp_data_dn.index = ramp_data_dn.index - pd.to_timedelta(
-            str(self.conf['ramps']['duration']))
+            str(self.ramps['duration']))
 
         ramp_df = (ramp_data_dn - self.ramp_data).dropna()
         zeros_col = np.zeros(len(ramp_df))
@@ -29,8 +37,8 @@ class r_magnitude:
         ramp_df['abs_diff_comp'] = zeros_col
 
         ramp_df.loc[abs(ramp_df[self.base_var])
-                    > self.conf['ramps']['magnitude'], ['abs_diff_base']] = 1
+                    > self.ramps['magnitude'], ['abs_diff_base']] = 1
         ramp_df.loc[abs(ramp_df[self.comp_var])
-                    > self.conf['ramps']['magnitude'], ['abs_diff_comp']] = 1
+                    > self.ramps['magnitude'], ['abs_diff_comp']] = 1
 
         return ramp_df
