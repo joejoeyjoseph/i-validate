bcbaeeaf29c69f9dccd9dd780f71e660f5dd1919
Lee <gdpp4352@gmail.com>
2021-06-29 19:47:00 -0600

    add dw

--- inputs/submission_csv.py
+++ inputs/submission_csv.py
@@ -10,11 +10,14 @@ class submission_csv:
     def __init__(self, info, conf):
 
         self.path = str(pathlib.Path(os.getcwd()).parent)+'/'+str(info['path'])
+        self.file = info['file']
         self.nature = info['nature']
+        self.target_var = info['target_var']
+        self.freq = info['freq']
 
     def get_ts(self, lev):
 
-        df_all = pd.read_csv(self.path+'/'+'benchmark_ICON2km_wfip2_sodar.csv')
+        df_all = pd.read_csv(self.path+'/'+self.file)
 
         if self.nature == 'ws':
             nature = 'speed'
@@ -23,8 +26,19 @@ class submission_csv:
 
         df = df_all[col]
 
-        df.set_index(df_all['time'], inplace=True)
+        if len(col) > 1:
+            print()
+            print('!!!!!!!!!!')
+            print('ERROR: SELECTING MULTIPLE COLUMNS')
+            print('!!!!!!!!!!')
+
+        df = df.rename(columns={col[0]: self.target_var})
+
+        df = df.set_index(df_all['time']).sort_index()
         df.index.rename('t', inplace=True)
         df.index = pd.to_datetime(df.index)
+
+        # Does not need check_input_data.verify_data_file_count()
+        # because 1 csv file contains all data points
 
         return df
