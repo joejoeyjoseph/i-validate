a08307501b3863630d3aeea94cb88861a7628556
joejoeyjoseph <gdpp4352@gmail.com>
2021-09-03 10:48:33 -0600

    sort index twice to avoid error

--- inputs/submission_csv.py
+++ inputs/submission_csv.py
@@ -1,6 +1,5 @@
 # This is a parser for benchmark exercise submissions in csv format
 
-import collections
 import os
 import pathlib
 import pandas as pd
@@ -59,6 +58,9 @@ class submission_csv:
         df = df.set_index(df_all[t_col]).sort_index()
         df.index.rename('t', inplace=True)
         df.index = pd.to_datetime(df.index)
+        # need to sort index again for all datetime format
+        # e.g. month-day-year vs day-month-year
+        df = df.sort_index()
 
         # Does not need check_input_data.verify_data_file_count()
         # because 1 csv file contains all data points
